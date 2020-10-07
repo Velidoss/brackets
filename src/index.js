50f30761dca68a9f8ebc733bd96d41c545bab546
Velidoss <velidoss11@gmail.com>
2020-10-07 10:01:39 +0300

    tast solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let continueIteration = true
+
+    function iterate (string){
+        for( let i = 0; i< bracketsConfig.length; i++){
+            for(let j =0 ; j< string.length; j++){
+                if(string[j] === bracketsConfig[i][1]){
+                    if(string[j-1] === bracketsConfig[i][0]){
+                        string = string.slice(0, j-1).concat(string.slice(j+1))
+                    }
+                }
+            }
+        }
+        continueIteration = str !== string;
+        str = string
+    }
+
+    while(continueIteration){
+        iterate(str)
+    }
+
+    return str.length === 0;
 }
